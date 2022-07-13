e76d252a54af20abcd40d12617e2c427b99890fc
AbePabbathi <AbePabbathi>
2022-07-13 21:15:00 +0000

    test

--- etl-functions/auditFields/addDates.py
+++ etl-functions/auditFields/addDates.py
@@ -2,5 +2,6 @@ from pyspark.sql.functions import lit
 from datetime import datetime
 # add load date
 def addLoadDate(df):
+    # test comment
     df = df.withColumn("_loadDate",lit(datetime.now()))
     return df
